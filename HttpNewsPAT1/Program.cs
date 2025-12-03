4f89957278d9fedf878982fedd10668610ec7450
qwelord <secondofall22@rambler.ru>
2025-12-03 15:09:58 +0500

    Готовое начало

--- HttpNewsPAT1/Program.cs
+++ HttpNewsPAT1/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace HttpNewsPAT1
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
+            WebRequest request = WebRequest.Create("http://localhost/main");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Console.Write(response.StatusDescription);
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            Console.WriteLine(responseFromServer);
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+            Console.Read();
         }
     }
 }
