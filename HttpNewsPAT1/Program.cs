785389b101323a90550fe51d8b1594d5cf169cc3
qwelord <secondofall22@rambler.ru>
2025-12-03 15:30:32 +0500

    Получение печеньки авторизации. Прохоров

--- HttpNewsPAT1/Program.cs
+++ HttpNewsPAT1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -13,6 +14,33 @@ namespace HttpNewsPAT1
         static void Main(string[] args)
         {
 
+            SingIn("user", "user");
+            Console.Read();
+        }
+
+        public static void SingIn(string Login,string Password)
+        {
+            string url = "http://localhost/ajax/login.php";
+            Debug.WriteLine($"Выполняем запрос: {url}");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.CookieContainer = new CookieContainer();
+            string postData = $"login={Login}&password={Password}";
+            byte[] Data = Encoding.ASCII.GetBytes(postData);
+            request.ContentLength = Data.Length;
+            using(var stream = request.GetRequestStream())
+            {
+                stream.Write(Data, 0, Data.Length);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse() ;
+                Debug.WriteLine($"Статус выполнения: {response.StatusCode}");
+                string responseFromServer = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                Console.WriteLine(responseFromServer);
+            }
+        }
+
+        public static void Open()
+        {
             WebRequest request = WebRequest.Create("http://localhost/main");
             HttpWebResponse response = (HttpWebResponse)request.GetResponse();
             Console.Write(response.StatusDescription);
